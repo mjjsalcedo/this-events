4c50c1109b9e3888cf80c19930b0f3f1e679ba3e
Mariel Salcedo <Mariel Salcedo>
2017-05-24 21:57:47 -1000

    secret menu revealed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 //Secret Menu Challenge!
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var revealSecret = document.getElementsByClassName("name");
 
+
+for(i = 0; i<revealSecret.length; i++){
+revealSecret[i].addEventListener("click", Toggle);
+}
+
+function Toggle(){
+	var secretItem = this.querySelectorAll(".menu")[0];
+	if (secretItem.style.display === "none") {secretItem.style.display = "block";
+}else {
+	secretItem.style.display = "none";
+	}
+}
